a1661eafd779ade24e98cb9032851324b39b47b6
Paul Vallas <vallasff@hotmail.com>
2025-03-18 13:22:26 +0200

    Add method that creates ships

--- classes/gameBoardClass.js
+++ classes/gameBoardClass.js
@@ -1,6 +1,16 @@
+import { Ship } from "./shipClass.js";
+
 class GameBoard {
 	constructor() {
 		this.board = new Array(10).fill().map(() => new Array(10).fill(""));
+	}
+
+	placeShips() {
+		const carrier = new Ship(5);
+		const battleship = new Ship(4);
+		const destroyer = new Ship(3);
+		const submarine = new Ship(3);
+		const patrolBoat = new Ship(2);
 	}
 }
 
