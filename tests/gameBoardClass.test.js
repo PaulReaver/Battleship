c9add245a8274b6393b4e115f9cdf8401a154820
Paul Vallas <vallasff@hotmail.com>
2025-03-21 23:33:49 +0200

    Add tests for ship placement

--- tests/gameBoardClass.test.js
+++ tests/gameBoardClass.test.js
@@ -2,26 +2,43 @@ import { describe, it, expect, beforeAll } from "bun:test";
 import { GameBoard } from "../classes/gameBoardClass.js";
 
 describe("GameBoard", () => {
-	let board;
+	let gameBoard;
 	beforeAll(() => {
-		board = new GameBoard().board;
+		gameBoard = new GameBoard();
 	});
 
 	it("Should return a 10x10 board", () => {
-		expect(board).toBeArrayOfSize(10);
+		expect(gameBoard.board).toBeArrayOfSize(10);
 	});
 
 	it("Should check if each array element is of 10x10 size", () => {
-		for (const row of board) {
+		for (const row of gameBoard.board) {
 			expect(row).toBeArrayOfSize(10);
 		}
 	});
 
 	it("Should check if each cell element is empty", () => {
-		for (const row of board) {
+		for (const row of gameBoard.board) {
 			for (const cell of row) {
 				expect(cell).toBeEmpty();
 			}
+		}
+	});
+
+	it("Should check if ships were place in the board", () => {
+		gameBoard.placeShips();
+		for (let i = 0; i < 5; i++) {
+			expect(gameBoard.board[0][i]).toBe("carrier");
+		}
+		for (let i = 0; i < 4; i++) {
+			expect(gameBoard.board[1][i]).toBe("battleship");
+		}
+		for (let i = 0; i < 3; i++) {
+			expect(gameBoard.board[2][i]).toBe("destroyer");
+			expect(gameBoard.board[3][i]).toBe("submarine");
+		}
+		for (let i = 0; i < 2; i++) {
+			expect(gameBoard.board[4][i]).toBe("patrol");
 		}
 	});
 });
