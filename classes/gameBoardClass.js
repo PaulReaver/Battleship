866d386639c72c21856a21bf0fb6ccce71b38798
Paul Vallas <vallasff@hotmail.com>
2025-03-21 23:34:24 +0200

    Add function for ship placement

--- classes/gameBoardClass.js
+++ classes/gameBoardClass.js
@@ -10,7 +10,23 @@ class GameBoard {
 		const battleship = new Ship(4);
 		const destroyer = new Ship(3);
 		const submarine = new Ship(3);
-		const patrolBoat = new Ship(2);
+		const patrol = new Ship(2);
+
+		for (let i = 0; i < 5; i++) {
+			this.board[0][i] = "carrier";
+		}
+
+		for (let i = 0; i < 4; i++) {
+			this.board[1][i] = "battleship";
+		}
+
+		for (let i = 0; i < 3; i++) {
+			this.board[2][i] = "destroyer";
+			this.board[3][i] = "submarine";
+		}
+		for (let i = 0; i < 2; i++) {
+			this.board[4][i] = "patrol";
+		}
 	}
 }
 
