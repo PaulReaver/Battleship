4029d453d98333b133328a2ccbc4edf4d2b5a185
Paul Vallas <vallasff@hotmail.com>
2025-03-22 13:09:51 +0200

    Throw error if ship length is not a a positive number

--- classes/shipClass.js
+++ classes/shipClass.js
@@ -1,5 +1,8 @@
 class Ship {
 	constructor(length) {
+		if (typeof length !== "number" || length <= 0) {
+			throw new Error("Ship length must be a positive number.");
+		}
 		this.length = length;
 		this.hits = 0;
 	}
