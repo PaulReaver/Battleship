724d4a994d96641e1273b2e06b80b60f8eac9361
Paul Vallas <vallasff@hotmail.com>
2025-03-24 14:13:49 +0200

    Create players and place ships

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log("Hello via Bun!");
+import { Player } from "./classes/playerClass.js";
+
+const playerHuman = new Player("Paul");
+const playerComputer = new Player("Computer", true);
+
+playerHuman.ownBoard.placeShips();
+playerComputer.ownBoard.placeShips();
