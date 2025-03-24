88a40155614ad6da32a4c6c22f32fe53e4b89d6c
Paul Vallas <vallasff@hotmail.com>
2025-03-24 14:42:57 +0200

    Call function to render cells

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 import { Player } from "./js/playerClass.js";
+import { renderGameBoard } from "./js/renderGameBoard.js";
 
 const playerHuman = new Player("Paul");
 const playerComputer = new Player("Computer", true);
+
+renderGameBoard(document.querySelector(".player-game-board"));
+renderGameBoard(document.querySelector(".computer-game-board"));
 
 playerHuman.ownBoard.placeShips();
 playerComputer.ownBoard.placeShips();
