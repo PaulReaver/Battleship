3b5b072f8d9ac5cef01f5257399605da1cb33951
Paul Vallas <vallasff@hotmail.com>
2025-03-28 12:09:20 +0200

    Change order of calls

--- index.js
+++ index.js
@@ -4,8 +4,14 @@ import { renderGameBoard } from "./js/renderGameBoard.js";
 const playerHuman = new Player("Paul");
 const playerComputer = new Player("Computer", true);
 
-renderGameBoard(document.querySelector(".player-game-board"));
-renderGameBoard(document.querySelector(".computer-game-board"));
-
 playerHuman.ownBoard.placeShips();
 playerComputer.ownBoard.placeShips();
+
+renderGameBoard(
+	document.querySelector(".player-game-board"),
+	playerHuman.ownBoard,
+);
+renderGameBoard(
+	document.querySelector(".computer-game-board"),
+	playerComputer.ownBoard,
+);
