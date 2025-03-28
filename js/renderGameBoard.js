c52fade89bed74aee85321fd57f2e0ed70ed4527
Paul Vallas <vallasff@hotmail.com>
2025-03-28 12:09:45 +0200

    Add code for ship cell

--- js/renderGameBoard.js
+++ js/renderGameBoard.js
@@ -1,7 +1,19 @@
-function renderGameBoard(gameBoard) {
-	for (let i = 0; i < 100; i++) {
-		const cell = document.createElement("div");
-		gameBoard.appendChild(cell);
+function renderGameBoard(uiBoard, ownBoard) {
+	for (let i = 0; i < 10; i++) {
+		for (let j = 0; j < 10; j++) {
+			const cell = document.createElement("div");
+			uiBoard.appendChild(cell);
+
+			if (
+				ownBoard.board[i][j] === "carrier" ||
+				ownBoard.board[i][j] === "battleship" ||
+				ownBoard.board[i][j] === "destroyer" ||
+				ownBoard.board[i][j] === "submarine" ||
+				ownBoard.board[i][j] === "patrol"
+			) {
+				cell.className = "ship";
+			}
+		}
 	}
 }
 
