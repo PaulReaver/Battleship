43e15cc0cd3d47d7f54ddb5d0fc63cdda92ea06b
Paul Vallas <vallasff@hotmail.com>
2025-04-07 12:30:48 +0300

    Attributes set for each cell

--- js/renderGameBoard.js
+++ js/renderGameBoard.js
@@ -1,7 +1,12 @@
 function renderGameBoard(uiBoard, ownBoard) {
+	if (uiBoard.hasChildNodes()) {
+		uiBoard.replaceChildren();
+	}
 	for (let i = 0; i < 10; i++) {
 		for (let j = 0; j < 10; j++) {
 			const cell = document.createElement("div");
+			cell.setAttribute("x", i);
+			cell.setAttribute("y", j);
 			uiBoard.appendChild(cell);
 
 			if (
