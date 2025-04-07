4696e9595935494cc0387f89e25a61b80abe4714
Paul Vallas <vallasff@hotmail.com>
2025-04-07 12:30:21 +0300

    Computer board now render correctly if player attacks

--- js/index.js
+++ js/index.js
@@ -4,14 +4,21 @@ import { renderGameBoard } from "./renderGameBoard.js";
 const playerHuman = new Player("Paul");
 const playerComputer = new Player("Computer", true);
 
+const computerUiBoard = document.querySelector(".computer-game-board");
+const playerUiBoard = document.querySelector(".player-game-board");
+
 playerHuman.ownBoard.placeShips();
 playerComputer.ownBoard.placeShips();
 
-renderGameBoard(
-	document.querySelector(".player-game-board"),
-	playerHuman.ownBoard,
-);
-renderGameBoard(
-	document.querySelector(".computer-game-board"),
-	playerComputer.ownBoard,
-);
+renderGameBoard(playerUiBoard, playerHuman.ownBoard);
+renderGameBoard(computerUiBoard, playerComputer.ownBoard);
+
+computerUiBoard.addEventListener("click", (e) => {
+	if (e.target.tagName === "DIV") {
+		const cell = e.target;
+		const x = cell.getAttribute("x");
+		const y = cell.getAttribute("y");
+		playerComputer.ownBoard.receiveAttack(x, y);
+		renderGameBoard(computerUiBoard, playerComputer.ownBoard);
+	}
+});
